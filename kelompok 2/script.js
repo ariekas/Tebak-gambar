5a459e20121ffa57ac4b363359eadedd6628bd31
ariekas <140879155+ariekas@users.noreply.github.com>
2023-07-29 14:18:38 +0700

    Add files via upload

---
+++ kelompok 2/script.js
@@ -0,0 +1,255 @@
+var score = 0;
+
+function soal1() {
+    var container = document.getElementById('container_soal1');
+    var jawaban = document.getElementById('Jawaban_soal1').value;
+    var soal_selanjutnya = document.getElementById('container_soal2')
+
+    if (jawaban == 'REFORMASI' || jawaban == 'reformasi' || jawaban == 'Reformasi') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+    
+}
+
+function soal2() {
+    var container = document.getElementById('container_soal2');
+    var jawaban = document.getElementById('Jawaban_soal2').value;
+    var soal_selanjutnya = document.getElementById('container_soal3')
+
+    if (jawaban == 'pemilu' || jawaban == 'PEMILU' || jawaban == 'Pemilu') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal3() {
+    var container = document.getElementById('container_soal3');
+    var jawaban = document.getElementById('Jawaban_soal3').value;
+    var soal_selanjutnya = document.getElementById('container_soal4')
+
+    if (jawaban == 'malari' || jawaban == 'MALARI' || jawaban == 'Malari') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal4() {
+    var container = document.getElementById('container_soal4');
+    var jawaban = document.getElementById('Jawaban_soal4').value;
+    var soal_selanjutnya = document.getElementById('container_soal5')
+
+    if (jawaban == 'kerusuhan 1998' || jawaban == 'KERUSUHAN 1998' || jawaban == 'Kerusuhan 1998') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal5() {
+    var container = document.getElementById('container_soal5');
+    var jawaban = document.getElementById('Jawaban_soal5').value;
+    var soal_selanjutnya = document.getElementById('container_soal6')
+
+    if (jawaban == 'perjanjian renville' || jawaban == 'PERJANJIAN RENVILLE' || jawaban == 'Perjanjian renville') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal6() {
+    var container = document.getElementById('container_soal6');
+    var jawaban = document.getElementById('Jawaban_soal6').value;
+    var soal_selanjutnya = document.getElementById('container_soal7')
+
+    if (jawaban == 'pki' || jawaban == 'PKI' || jawaban == 'Pki') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal7() {
+    var container = document.getElementById('container_soal7');
+    var jawaban = document.getElementById('Jawaban_soal7').value;
+    var soal_selanjutnya = document.getElementById('container_soal8')
+
+    if (jawaban == 'konferensi meja bundar' || jawaban == 'KONFERENSI MEJA BUNDAR' || jawaban == 'Konferensi meja bundar') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal8() {
+    var container = document.getElementById('container_soal8');
+    var jawaban = document.getElementById('Jawaban_soal8').value;
+    var soal_selanjutnya = document.getElementById('container_soal9')
+
+    if (jawaban == 'pesawat pertama indonesia' || jawaban == 'pesawat N250' || jawaban == 'PESAWAT PERTAMA INDONESIA') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal9() {
+    var container = document.getElementById('container_soal9');
+    var jawaban = document.getElementById('Jawaban_soal9').value;
+    var soal_selanjutnya = document.getElementById('container_soal10')
+
+    if (jawaban == 'perjanjian linggar jati' || jawaban == 'PERJANJIAN LINGGAR JATI' || jawaban == 'Perjanjian linggar jati') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal10() {
+    var container = document.getElementById('container_soal10');
+    var jawaban = document.getElementById('Jawaban_soal10').value;
+    var soal_selanjutnya = document.getElementById('container_soal11')
+
+    if (jawaban == 'transmigrasi' || jawaban == 'Transmigrasi' || jawaban == 'TRASMIGRASI') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal11() {
+    var container = document.getElementById('container_soal11');
+    var jawaban = document.getElementById('Jawaban_soal11').value;
+    var soal_selanjutnya = document.getElementById('container_soal12')
+
+    if (jawaban == 'tritura' || jawaban == 'Tritura' || jawaban == 'TRITURA') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal12() {
+    var container = document.getElementById('container_soal12');
+    var jawaban = document.getElementById('Jawaban_soal12').value;
+    var soal_selanjutnya = document.getElementById('container_soal13')
+
+    if (jawaban == 'soeharto' || jawaban == 'SOEHARTO' || jawaban == 'Soeharto') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal13() {
+    var container = document.getElementById('container_soal13');
+    var jawaban = document.getElementById('Jawaban_soal13').value;
+    var soal_selanjutnya = document.getElementById('container_soal14')
+
+    if (jawaban == 'supersemar' || jawaban == 'SUPERSEMAR' || jawaban == 'Supersemar') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal14() {
+    var container = document.getElementById('container_soal14');
+    var jawaban = document.getElementById('Jawaban_soal14').value;
+    var soal_selanjutnya = document.getElementById('container_soal15')
+
+    if (jawaban == 'nawaksara' || jawaban == 'NAWAKSARA' || jawaban == 'Nawaksara') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
+
+function soal15() {
+    var container = document.getElementById('container_soal15');
+    var jawaban = document.getElementById('Jawaban_soal15').value;
+    var soal_selanjutnya = document.getElementById('container_selesai')
+
+    if (jawaban == 'bj habibie' || jawaban == 'BJ HABIBIE' || jawaban == 'Bj habibie') {
+        container.setAttribute('class', 'hidden');
+        soal_selanjutnya.removeAttribute('class');
+        score = score + 20;
+
+        alert('Benar!! Kamu Pintar!!')
+    } else {
+        alert('Kamu Salah, Coba Lagi!!')
+    }
+
+}
